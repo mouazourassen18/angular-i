5e99c3beddc36160a45c20646118932e99f5b40c
mouazourassen18 <54610297+mouazourassen18@users.noreply.github.com>
2021-09-17 12:29:01 -0400

    another updated version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, VERSION } from '@angular/core';
 
-<h1 class="text-italic">
+@Component({
+  selector: 'my-app',
+  template: `
+
+  <h1 class="text-italic">
       Heloo class="text-italic" from style
     </h1>
     <h2 [class]="teRed">
@@ -41,12 +45,31 @@ import { Component, VERSION } from '@angular/core';
     <h2>{{i}} :  {{coco}} : {{l}}  : {{j}}</h2>
     </div>
   `,
-  ]
+  styles: [
+    `
+      h1 {
+        color: green;
+      }
+      .text-red {
+        color: red;
+      }
+
+      .text-yellow {
+        color: yellow;
+      }
+      .text-italic {
+        font-style: italic;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
   public tex = 'text-italic';
   teRed = 'text-red';
+  tOf = false;
+  colors = ['red', 'green', 'yellow', 'brown', 'blue'];
+  color = 'yellow';
 }
 //  either  you can use the normal classes , you ablecan use a reference to   a variable// from the classes that you can use from  the   style  --> to the sheet .
 // if you want to use multiple  classes at the ssame time
