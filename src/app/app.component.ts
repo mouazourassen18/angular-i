e3495e373e254b29aa617ea38e7390beb47e857d
mouazourassen18 <54610297+mouazourassen18@users.noreply.github.com>
2021-09-17 12:27:44 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component, VERSION } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  template: `
-    <h1 class="text-italic">
+<h1 class="text-italic">
       Heloo class="text-italic" from style
     </h1>
     <h2 [class]="teRed">
@@ -12,23 +9,38 @@ import { Component, VERSION } from '@angular/core';
     <h2 [style.color]="'blue'">
       this is a red color[class]=teRed as a reference from class
     </h2>
-  `,
-  styles: [
-    `
-      h1 {
-        color: green;
-      }
-      .text-red {
-        color: red;
-      }
+    <h1 *ngIf ="true">
+    Hello  World !
+    </h1> 
 
-      .text-yellow {
-        color: yellow;
-      }
-      .text-italic {
-        font-style: italic;
-      }
-    `
+   
+    <br>
+    <div  *ngIf = " tOf ; then thnBlock ;  else elBlock "> </div>  
+
+    
+
+    <ng-template #elBlock >
+    <input  type="text"  placeholder="second">
+    
+    </ng-template>
+    <ng-template #thnBlock >
+    <input  type="text"  placeholder ="hellooo">
+    <br>
+    <button> click </button>
+    </ng-template>
+   
+
+    <div [ngSwitch]= color>
+    <div *ngSwitchCase ="'red'"> this is just red , we are diplaying </div>
+    <div *ngSwitchCase ="'green'"> <h3>this is just green, we are diplaying </h3></div>
+    <div *ngSwitchCase ="'blue'"> this is just blue , we are diplaying </div>
+    <div *ngSwitchDefault> <h2> Pick another color</h2> </div>
+    </div>
+
+    <div *ngFor =" let coco of colors ; index as i ; odd as l ; even as j">
+    <h2>{{i}} :  {{coco}} : {{l}}  : {{j}}</h2>
+    </div>
+  `,
   ]
 })
 export class AppComponent {
